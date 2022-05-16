c400d5f00458ace6c22923c291e35c84d6565d04
Ubuntu <ubuntu@ip-172-31-47-184.ap-northeast-2.compute.internal>
2022-05-16 07:27:17 +0000

    adder 수정

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -12,22 +12,21 @@ int main(void)
 
     // 두개 뽑아내기
     if ((buf = getenv("QUERY_STRING")) != NULL){
-        // buf에서 & 인덱스 찾기
         p = strchr(buf, '&');
-        *p = '\0';// & 위치를 NULL로 채워줘서 문자열 자르기(?)
-        strcpy(arg1,buf);
-        strcpy(arg2,p+1);
-        // char to int
+        *p = '\0';
+        strcpy(arg1, buf+6);
+        strcpy(arg2, p+8);
         n1 = atoi(arg1);
         n2 = atoi(arg2);
     }
+
     //response body만들기
     sprintf(content, "QUERY_STRING=%s", buf);
-    sprintf(content, "Welcome to add.com: ");
-    sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
-    sprintf(content, "%sThe answer is : %d + %d = %d\r\n<p>",
-            content, n1,n2,n1+n2);
-    sprintf(content, "%sThanks for visiting!\r\n", content);
+    sprintf(content, "Welcome: ");
+    sprintf(content, "%sTHE Internet addition portal. \r\n<p>", content);
+    sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
+    sprintf(content, "%sThanks!\r\n", content);
+
     
     // HTTP response 만들기
     printf("Connection : close\r\n");
